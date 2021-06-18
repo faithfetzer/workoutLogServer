6a02a93b4789c9a7133c6df25087a2cdcad731e7
Faith Fetzer <faithfetzer@Faiths-MacBook-Air.local>
2021-06-18 13:14:11 -0400

    rest of modules

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -5,7 +5,17 @@ let middleware = require('../middleware');
 
 
 router.get("/", middleware.validateSession, async (req, res) =>{
-    res.send('this is a test')
+    try{
+        console.log('user info', req.user.id);
+        const allLogs = await LogModel.findAll(
+            {where: {owner_id: req.user.id}}
+        );
+        res.status(200).json(allLogs);
+    } catch(err){
+        res.status(500).json({
+            error: err
+        })
+    }
 });
 
 router.post("/", middleware.validateSession, async (req, res) =>{
@@ -17,25 +27,76 @@ router.post("/", middleware.validateSession, async (req, res) =>{
         result,
         owner_id: id
     }
+    console.log(req.body);
     try {
-        // await LogModel.sync({ alter: true })
         const newLog = await LogModel.create(logEntry);
         res.status(200).json(newLog)
     } catch(err){
         res.status(500).json({
+            logEntry,
             error: err,
             user_id: id
         }
 
         )
     }
-    // LogModel.create(logEntry)
 });
 
 
 
-// router.get("/:id", async (req, res) =>{
+router.get("/:id", middleware.validateSession, async (req, res) =>{
+    try{ 
+        const locatedLog = await LogModel.findOne({
+        where: {id: req.params.id}
+    })
+    res.status(200).json({
+        log: locatedLog
+    })
+    } catch(err) {
+        res.status(500).json({
+            message: `Failed to retrieve log: ${err}`
+        })
 
-// });
+    }
+});
 
+router.put("/:id", middleware.validateSession, async(req, res) =>{
+    const {description, definition, result} = req.body.log;
+    // const {id} = req.user;
+    try{
+        const updatedLog = await LogModel.update({
+            description,
+            definition,
+            result,
+            owner_id: req.user.id
+        },
+            {where: {id: req.params.id}}
+            )
+
+            res.status(200).json({
+                updatedLog
+            })
+    } catch(err) {
+        res.status(500).json({
+            message:`failed to update log: ${err}`
+        })
+
+    }
+});
+
+router.delete("/:id", middleware.validateSession, async (req, res) => {
+    try{
+        const locatedLog = await LogModel.destroy({
+            where: {id: req.params.id}
+        })
+        res.status(200).json({
+            deletedLog: locatedLog
+            })
+        } catch(err) {
+            res.status(500).json({
+                message: `Failed to delete log: ${err}`
+            })
+        }
+    }
+);
 module.exports = router;
--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -3,27 +3,24 @@ const jwt = require("jsonwebtoken");
 const {UserModel} = require('../models');
 
 const validateSession = async (req, res, next) =>{
-    console.log(req.method == "OPTIONS");
-    console.log(req.headers.authorization);
-    if(req.method == "OPTIONS"){
+    // console.log(req.method == "OPTIONS");
+    // console.log(req.headers.authorization);
+    if(req.method === "OPTIONS"){
         next()
-   // } else if(req.headers.authorization &&
-        //req.headers.authorization.includes("Bearer")){
-       // const {authorization} = req.headers;
-       // console.log("authorization-->", authorization);
-       // const payload = authorization ? jwt.verify(authorization.includes("Bearer") ? authorization.split("")[1]: authorization, process.env.JWT_SECRET) : //undefined;
-       // console.log("payload -->", payload);
-        //if(payload){
     }else if (req.headers.authorization){
+        // console.log("test",req.headers.authorization)
         const {authorization} = req.headers;
+        // console.log("authorization", authorization);
         const payload = authorization ? jwt.verify(authorization, process.env.JWT_SECRET) : undefined;
+        // console.log("payload", payload);
         if(payload){
             let foundUser = await UserModel.findOne({
-                where: {id: payload.id}
+                where: {id: payload.id},
+                message: req.body
             });
-            console.log("foundUser -->", foundUser);
+            
             if(foundUser){
-                console.log("request -->", req);
+                // console.log("request -->", req.body);
                 req.user = foundUser;
                 next()
             } else {
